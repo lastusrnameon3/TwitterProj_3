c79ddcc399d40291108a8f3e98d4d33966068828
lastusrnameonearth@gmail.com <lastusrnameonearth@gmail.com>
2019-01-04 15:14:02 -0600

    Firs t Push of 1st Project

---
+++ app/models/tweet.rb
@@ -0,0 +1,10 @@
+class Tweet < ApplicationRecord
+  
+
+  validates :message, presence: true
+  validates :message, length: {maximum: 140, 
+	too_long: "A tweet is only 140 max. Everybody knows that!"}
+
+belongs_to :user
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+
+  Rails.application.routes.draw do
+  root 'epicenter#feed'
+
+  get 'show_user' => 'epicenter#show_user'
+
+  get 'now_following' => 'epicenter#now_following'
+
+  get 'unfollow' => 'epicenter#unfollow'
+
+  resources :tweets
+  devise_for :users
+end
+
+  # get 'epicenter/feed'
+  # get 'epicenter/show_user'
+  # get 'epicenter/now_following'
+  # get 'epicenter/unfollow'
+  # resources :tweets
+  # devise_for :views
+  # devise_for :users
+
+  # root 'Epicenter#feed'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
